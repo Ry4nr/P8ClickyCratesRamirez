25f54aaf660a87a00d21739096352d297789a3d0
Ry4nr <756238@student.dusd.net>
2025-05-20 15:28:48 -0700

    added satuff
    yy e waw

--- ClickyCratesRamirezP8/Assets/Scripts/Target.cs
+++ ClickyCratesRamirezP8/Assets/Scripts/Target.cs
@@ -5,18 +5,23 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
     private Rigidbody targetRb;
+    private GameManager gameManager;
     private float minSpeed = 12;
     private float maxSpeed = 16;
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -6;
+    public ParticleSystem explosionParticle;
+    public int pointValue;
     // Start is called before the first frame update
     void Start()
     {
         targetRb = GetComponent<Rigidbody>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
 
         targetRb.AddForce (RandomForce(), ForceMode.Impulse);
         targetRb.AddTorque(Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque), Random.Range(-maxTorque, maxTorque));
+        
         transform.position = new Vector3(Random.Range(-xRange, xRange),ySpawnPos);
     }
     Vector3 RandomForce()
@@ -30,6 +35,18 @@ public class Target : MonoBehaviour
     Vector3 RandomSpanPos()
     {
         return new Vector3(Random.Range(-xRange,xRange),ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
     // Update is called once per frame
     void Update()
